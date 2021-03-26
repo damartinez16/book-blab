e2642ce2772862452ebaffdf6b9d213c7ccac2b0
Danielle Martinez <daniellealexandria10@gmail.com>
2021-03-26 13:41:21 -0600

    models

---
+++ models/book.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    content: String,
+    rating: {
+        type: Number,
+        min: 1,
+        max:5,
+        default: 5
+    },
+    userName: String,
+    userAvatar: String
+}, {
+    timestamps: true
+});
+
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: String,
+    comments: [commentSchema]
+}, {
+    timestamps: true
+});
