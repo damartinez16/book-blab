ebdd94b3cb0cf9d662293c3557191df9f44c2c7e
Danielle Martinez <daniellealexandria10@gmail.com>
2021-03-26 13:15:42 -0600

    google oauth

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var methodOverride = require('method-override');
 
 require('dotenv').config();
 require('./config/database');
+require('./config/passport');
 
 var indexRouter = require('./routes/index');
 var booksRouter = require('./routes/books');
@@ -34,6 +35,11 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(function(req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/books', booksRouter);
